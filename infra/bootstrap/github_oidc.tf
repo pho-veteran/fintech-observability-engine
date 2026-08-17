6a11be4d4e067de826b55e24add31769900d81a3
pho-veteran <thanhvinhlqt2016@gmail.com>
2026-08-17 12:16:46 +0700

    fix: trust GitHub repository OIDC contexts

--- infra/bootstrap/github_oidc.tf
+++ infra/bootstrap/github_oidc.tf
@@ -62,9 +62,9 @@ data "aws_iam_policy_document" "github_oidc_trust" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = local.github_oidc_allowed_subjects
+      values   = ["repo:${var.github_owner}/${var.github_repo}:*"]
     }
   }
 }
